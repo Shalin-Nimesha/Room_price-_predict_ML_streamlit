3502e1de28f70b6d0ae9df0a2e1d9acce0e44ce6
Shalin-Nimesha <shalinnimesha8@gmail.com>
2025-10-18 15:57:12 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,35 @@
+import pandas as pd
+from sklearn.linear_model import LinearRegression
+from sklearn.model_selection import train_test_split
+import joblib
+
+# Dummy dataset
+data = {
+    'area': [1000, 1500, 2000, 2500, 3000],
+    'bedrooms': [2, 3, 3, 4, 4],
+    'age': [10, 15, 20, 5, 8],
+    'price': [100000, 150000, 200000, 250000, 270000]
+}
+df = pd.DataFrame(data)
+X = df[['area', 'bedrooms', 'age']]
+y = df['price']
+model = LinearRegression()
+model.fit(X, y)
+# Save model
+joblib.dump(model, 'model.pkl')
+
+import streamlit as st
+import joblib
+import numpy as np
+# Load model
+model = joblib.load("model.pkl")
+st.title(" House Price Prediction App")
+st.write("Enter house details to predict the price:")
+# Inputs
+area = st.number_input("Area (sq ft)", value=1000)
+bedrooms = st.number_input("Number of Bedrooms", value=2, step=1)
+age = st.number_input("Age of House (years)", value=10)
+if st.button("Predict Price"):
+ features = np.array([[area, bedrooms, age]])
+ prediction = model.predict(features)
+ st.success(f"Estimated House Price: ${prediction[0]:,.2f}")
